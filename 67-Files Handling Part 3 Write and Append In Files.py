aa7da8f15343680e85d3e991365bf62973f7f8c5
Hussien Yasser <husienyaser2004@gmail.com>
2026-08-03 23:47:59 +0200

    67-Files Handling Part 3 Write and Append In Files.py

---
+++ 67-Files Handling Part 3 Write and Append In Files.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#-Files Handling Part 3 Write and Append In Files-
+#-------------------------------------------------
+
+#myfile = open("hussien.txt.txt", "w") 
+#myfile.write("Hello Hussien\n") # Write In File
+#myfile.write("Third Line") # Write In File
+
+#myfile = open("fun.txt", "w")
+#myfile.write("Hussien Soudy\n"* 100) # Write In File
+
+#myList = ["Hussien Soudy\n", "Ahmed\n", "Mohamed\n"]
+
+#myfile = open("hussien.txt.txt", "w")
+#myfile.writelines(myList) # Write In File
+
+myfile = open("hussien.txt.txt", "a") 
+myfile.write("Hello Hussien\n") # Write In File
+myfile.write("Soudy") # Write In File
+
