2922f39428ad864df806483cc66696e94b00a429
Hussien Yasser <husienyaser2004@gmail.com>
2026-08-05 00:05:24 +0200

    68-Files Handling Part 4 Important Info.py

---
+++ 68-Files Handling Part 4 Important Info.py
@@ -0,0 +1,16 @@
+#-------------------------------------------
+#---Files Handling Part 4 Important Info----
+#-------------------------------------------
+
+#myfile = open("hussien.txt", "a")
+#myfile.truncate(5)
+
+#myfile = open("hussien.txt", "a")
+#print(myfile.tell()) # Get Current Position In File
+
+#myfile = open("hussien.txt", "r")
+#myfile.seek(5)
+#print(myfile.read())
+
+#import os
+#os.remove("hussien.txt") # Delete File
