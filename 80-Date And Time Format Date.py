5f2e132585473b89a075323e9ffd19c78a8c1109
Hussien Yasser <husienyaser2004@gmail.com>
2026-08-23 20:49:52 +0200

    80-Date And Time Format Date.py

---
+++ 80-Date And Time Format Date.py
@@ -0,0 +1,25 @@
+#----------------------------------
+#-- Date and Time => Format Date --
+#----------------------------------
+#https://strftime.org/-------------
+#----------------------------------
+
+import datetime
+
+myBirthday = datetime.datetime(2004, 10, 21)
+
+print(myBirthday)
+print(myBirthday.strftime("%a"))
+print(myBirthday.strftime("%A"))
+print(myBirthday.strftime("%b"))
+print(myBirthday.strftime("%B"))
+
+print(myBirthday.strftime("%d %B %Y"))
+print(myBirthday.strftime("%d, %B, %Y"))
+print(myBirthday.strftime("%d/%B/%Y"))
+print(myBirthday.strftime("%d - %B - %Y"))
+
+
+
+
+ 
