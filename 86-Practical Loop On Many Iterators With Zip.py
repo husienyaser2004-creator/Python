bee3af87d6a3951e42a983caecc415bceb62360e
Hussien Yasser <husienyaser2004@gmail.com>
2026-08-30 23:30:17 +0200

    86-Practical Loop On Many Iterators With Zip.py

---
+++ 86-Practical Loop On Many Iterators With Zip.py
@@ -0,0 +1,31 @@
+#-------------------------------------------------
+#------Practical Loop On Many Iterators With Zip--
+#-------------------------------------------------
+# zip() Return A Zip Object Contains All Objects
+# zip() Length Is The Length Of Lowest Object 
+#-------------------------------------------------
+
+list1 = [1, 2, 3, 4, 5]
+list2 = ["A", "B", "c", "D"]
+tuple1 = ("Man", "Woman", "Girl", "Boy")
+dict1 = {"Name": "Osama", "Age":36, "Country": "Egypt", "Skill": "Python"}
+
+for item1, item2, item3, item4 in zip(list1, list2, tuple1, dict1):
+
+    print("List 1 Item =>", item1)
+    print("List 2 Item =>", item2)
+    print("Tuple 1 Item =>", item3)
+    print("Dict 1 Key =>", item4, "Value =>", dict1[item4])
+
+
+
+# list = [1, 2, 3, 4, 5]
+# list = ["A", "B", "c"]
+
+# ultimatelist = zip(list1, list2)
+
+# print(ultimatelist)
+
+# for item in ultimatelist :
+
+# print (item)        
